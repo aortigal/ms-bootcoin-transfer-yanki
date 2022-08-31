820d071659208b5ed8cc727ffa18d7b06efe5fd0
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-30 22:10:13 -0500

    add structure base

---
+++ src/test/java/com/bank/msbootcointransferyanki/MsBootcoinTransferYankiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcointransferyanki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcoinTransferYankiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
